1e28c581bdc0fb1ed816fd85517fe83bd89afb98
tenguyen <te.nm@urbox.vn>
2024-04-09 17:50:30 +0700

    fix: remove log

--- crypt/aes.go
+++ crypt/aes.go
@@ -19,14 +19,14 @@ func AESEncrypt(key []byte, text []byte) (string, error) {
 
 	// Padding văn bản
 	text = pad(text)
-	log.Print(aes.BlockSize + len(text))
+	// log.Print(aes.BlockSize + len(text))
 	ciphertext := make([]byte, aes.BlockSize+len(text))
 	iv := ciphertext[:aes.BlockSize]
 
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return "", err
 	}
-	log.Print(string(iv))
+	// log.Print(string(iv))
 	mode := cipher.NewCBCEncrypter(block, iv)
 	mode.CryptBlocks(ciphertext[aes.BlockSize:], text)
 
@@ -51,7 +51,7 @@ func AESDecrypt(key []byte, encryptedText string) ([]byte, error) {
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 
-	log.Print(string(iv))
+	// log.Print(string(iv))
 
 	mode := cipher.NewCBCDecrypter(block, iv)
 	mode.CryptBlocks(ciphertext, ciphertext)
